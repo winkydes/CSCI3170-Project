e1bb86d7207147b47a7a7d1ab411f0eedabd73c0
Keith Lam <keithlam0110@gmail.com>
2022-11-28 18:19:09 +0800

    basic setup

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-//HelloWorld.java
-
-class HelloWorld {
-    public static String mesg = "Hello World!";
-    public static void main(String[] args) {
-        System.out.println(mesg);
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//HelloWorld.java
+
+class HelloWorld {
+    public static String mainMenuMsg = "Welcome to sales system! \n \n-----Main menu-----\nWhat kinds of operation would you like to perform?\n1. Operations for administrator\n2. Operations for salesperson\n3. Operations for manager\n4. Exit this program\nEnter Your Choice: ";
+    public static void main(String[] args) {
+        Integer choice = 0;
+        Scanner choiceScanner = new Scanner(System.in);
+        while (choice != 4) {
+            System.out.println(mainMenuMsg);
+            choice = choiceScanner.nextInt();
+            switch (choice) {
+                case 1: 
+            }
+        }
+        choiceScanner.close();
+    }
+}
